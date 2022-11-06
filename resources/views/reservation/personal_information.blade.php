49de9703b311743d994bc58762a8b8243fb29da3
Rei0825 <maso.akiha@icloud.com>
2022-11-06 07:30:15 +0000

    personal_information

---
+++ resources/views/reservation/personal_information.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>customer_reservation</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h2>Reservation System</h2>
+            <div class='personal_information'>
+                <h1>人数</h1>
+                <h1>氏名</h1>
+                <h1>Tel</h1>
+                <h1>e-mail</h1>
+                <h1>備考</h1>
+            </div>
+        <h3>戻る</h3>
+        <h3>確認画面へ</h3>
+        </div>
+    </body>
+</html>
